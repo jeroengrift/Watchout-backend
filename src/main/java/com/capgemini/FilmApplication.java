f734f6d80b757360b6f49fc01acb1d4f7bfea384
jeroengrift <jeroengrift@outlook.com>
2018-11-22 12:54:26 +0100

    login

--- src/main/java/com/capgemini/FilmApplication.java
+++ src/main/java/com/capgemini/FilmApplication.java
@@ -2,11 +2,17 @@ package com.capgemini;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan({"com.capgemini.service", "com.capgemini.data", "com.capgemini.web"})
+@EnableJpaRepositories("com.capgemini.data")
+@EntityScan( basePackages = {"com.capgemini.domain"} )
 @SpringBootApplication
 public class FilmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmApplication.class, args);
 	}
-}
+	}
